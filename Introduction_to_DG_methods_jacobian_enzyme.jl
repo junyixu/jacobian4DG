d41a2a576a5c3f90fa6093350a0b96293cdb5c1e
Junyi Xu <junyixu0@gmail.com>
2024-06-17 12:03:44 +0800

    add jacobian_ad_forward_enzyme for  Introduction to DG methods
    https://trixi-framework.github.io/Trixi.jl/dev/tutorials/scalar_linear_advection_1d/#Alternative-Implementation-based-on-Trixi.jl-2

---
+++ Introduction_to_DG_methods_jacobian_enzyme.jl
@@ -0,0 +1,46 @@
+#! /usr/bin/env -S julia --color=yes --startup-file=no
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#=
+    https://trixi-framework.github.io/Trixi.jl/dev/tutorials/differentiable_programming/#Computing-the-Jacobian
+=#
+
+using Trixi
+using Enzyme
+
+# %%
+# equation with a advection_velocity of `1`.
+advection_velocity = 1.0
+equations = LinearScalarAdvectionEquation1D(advection_velocity)
+
+# create DG solver with flux lax friedrichs and LGL basis
+solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs)
+
+# distretize domain with `TreeMesh`
+coordinates_min = -1.0 # minimum coordinate
+coordinates_max = 1.0 # maximum coordinate
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=4, # number of elements = 2^4
+                n_cells_max=30_000)
+
+# create initial condition and semidiscretization
+initial_condition_sine_wave(x, t, equations) = SVector(1.0 + 0.5 * sin(pi * sum(x - equations.advection_velocity * t)))
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_sine_wave, solver)
+# %%
+J1 = jacobian_ad_forward(semi);
+
+function jacobian_ad_forward_enzyme(semi)
+    t0 = zero(real(semi))
+    u_ode = compute_coefficients(t0, semi)
+    du_ode = similar(u_ode)
+    dy = Tuple(zeros(size(du_ode)) for _ in 1:length(u_ode))
+    dx = Enzyme.onehot(u_ode)
+    tuple_semi=Tuple(Enzyme.make_zero(semi) for i=1:length(u_ode))
+    Enzyme.autodiff(Enzyme.Forward, (du_ode, u_ode, semi)->Trixi.rhs!(du_ode, u_ode, semi, t0), Enzyme.BatchDuplicated(du_ode, dy), Enzyme.BatchDuplicated(u_ode, dx), BatchDuplicated(semi, tuple_semi))
+    return stack(dy)
+end
+
+J2 = jacobian_ad_forward_enzyme(semi)
+
+J1 == J2 # ture
